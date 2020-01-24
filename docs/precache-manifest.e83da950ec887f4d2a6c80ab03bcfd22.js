85cf724c089166d4212c8d5a01c807077d093720
RomanovAleksander <1eksandr999@gmail.com>
2020-01-24 16:46:23 +0200

    homapage updated

---
+++ docs/precache-manifest.e83da950ec887f4d2a6c80ab03bcfd22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a3df965eec49b2651778cbbb5abab0",
+    "url": "/BlogReactApp/index.html"
+  },
+  {
+    "revision": "f4b8e0f4b84a5d3cee35",
+    "url": "/BlogReactApp/static/css/main.1470bcd6.chunk.css"
+  },
+  {
+    "revision": "2a2ae8ea0bad230e40ec",
+    "url": "/BlogReactApp/static/js/2.9fd93fb1.chunk.js"
+  },
+  {
+    "revision": "f4b8e0f4b84a5d3cee35",
+    "url": "/BlogReactApp/static/js/main.584737ea.chunk.js"
+  },
+  {
+    "revision": "37bf4e10a3103d0780cf",
+    "url": "/BlogReactApp/static/js/runtime-main.2eaff749.js"
+  }
+]);
